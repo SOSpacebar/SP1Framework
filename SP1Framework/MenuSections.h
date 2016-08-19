95cd8edb0320afb3ed5d122e5be289354db59480
Lim Yi Chun <hayashikiyoi@gmail.com>
2016-08-19 13:02:44 +0800

    Setting Menu into other CPP file

---
+++ SP1Framework/MenuSections.h
@@ -0,0 +1,17 @@
+#include "Framework\timer.h"
+#include "Framework\console.h"
+#include "game.h"
+#include <string>
+
+
+extern CStopWatch g_swTimer;
+extern bool g_bQuitGame;
+extern Console g_Console;
+using namespace std;
+
+void renderMainMenu();// Main Menu
+void LevelSelect();// Selecting Level
+void SetAnimationSplashScreen(); // Showing Animation
+void DrawAnimationSplashScreen(); // Draw the SplashScreen in
+void GameOver(); // When Player got killed in the game
+void Credits(); // Crediting everyone who work for the game
