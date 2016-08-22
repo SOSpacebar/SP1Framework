08d0287f816447bdc0f36c145906f19bc35fb51b
Lim Pei Sheng <peisheng1@hotmail.com>
2016-08-22 18:04:23 +0800

    added random generated gameObject

--- SP1Framework/gameObject.h
+++ SP1Framework/gameObject.h
@@ -10,31 +10,26 @@
 
 using namespace std;
 
-int randomArr(int rand_ID)
-{
-	rand_ID = rand() % 5;
-}
-
 struct objectStruct
 {
 	COORD o_start_location;
 	COORD o_location;
 	string o_ID;
 	int o_speed;
-	int o_distance;
 	bool o_reset;
 };
 
 void init_object(short level);				//GameObject initialiser
 void update_GameObject(void);				//GameObject Indivisual function call
 
-void updateLR_Projectile(string &ID, COORD &start_xy, COORD &xy, int &dist, int &speed, bool &reset);					//Left to Right Projectile
-void updateRL_Projectile(string &ID, COORD &start_xy, COORD &xy, int &dist, int &speed, bool &reset);					//Right to Left Projectile
-void updateUD_Projectile(string &ID, COORD &start_xy, COORD &xy, int &dist, int &speed, bool &reset);					//Up to Down Projectile
-void updateDU_Projectile(string &ID, COORD &start_xy, COORD &xy, int &dist, int &speed, bool &reset);					//Down to Up Projectile
+void updateLR_Projectile(string &ID, COORD &start_xy, COORD &xy, int &speed, bool &reset);					//Left to Right Projectile
+void updateRL_Projectile(string &ID, COORD &start_xy, COORD &xy, int &speed, bool &reset);					//Right to Left Projectile
+void updateUD_Projectile(string &ID, COORD &start_xy, COORD &xy, int &speed, bool &reset);					//Up to Down Projectile
+void updateDU_Projectile(string &ID, COORD &start_xy, COORD &xy, int &speed, bool &reset);					//Down to Up Projectile
 
 void updateLR_EBall(string &ID, COORD &start_xy, COORD &xy, int &speed, bool &reverse);						//Right Left Crushing Block
 void updateUD_EBall(string &ID, COORD &start_xy, COORD &xy, int &speed, bool &reverse);
 void findCoordStart(int x, int y);
+int randomArr(int rand_vec_size);
 
 #endif
