dfd2c7dfb6c6a5cf1b373e95f3a8e1f63f140239
Ng Jing Jie <1n506@CL36G203.sit.nyp.edu.sg>
2016-08-24 12:55:13 +0800

    Bullet and portal will now reveal map.

--- SP1Framework/fieldOfView.cpp
+++ SP1Framework/fieldOfView.cpp
@@ -2,24 +2,27 @@
 
 COORD c;
 
-void renderFogOfWarAndMap(SMapData &g_mapData, SGameChar player, Console &g_Console, char fogMap[150][150])
+void renderFogOfWarAndMap(SMapData &g_mapData, SGameChar player, Console &g_Console, char fogMap[150][150], Bullet &_bullet, Portal &_portal)
 {
+	//Coord Variables
 	c.X = 0;
 	c.Y = 1;
 	int tempValue = c.X;
 
 	char fogMap2[150][150];
 
+	//View Range Variables
 	short viewRange = 6;
-	short x;
-	short y;
+	short portalViewRange = 3;
+	short bulletViewRange = 2;
+
 
 	memset(fogMap2, '\0', sizeof(fogMap2[0][0]) * 150 * 150);
 	
-
-	for (x = player.m_cLocation.X - viewRange - 1; x < player.m_cLocation.X + viewRange + 1; x++)
+	//Check player position for rendering
+	for (short x = player.m_cLocation.X - viewRange - 1; x < player.m_cLocation.X + viewRange + 1; x++)
 	{
-		for (y = player.m_cLocation.Y - viewRange; y < player.m_cLocation.Y + viewRange - 2; y++)
+		for (short y = player.m_cLocation.Y - viewRange; y < player.m_cLocation.Y + viewRange - 2; y++)
 		{
 			if (x < 0)
 			{
@@ -32,6 +35,72 @@ void renderFogOfWarAndMap(SMapData &g_mapData, SGameChar player, Console &g_Cons
 
 			fogMap[y][x] = g_mapData.mapGrid[y][x];
 			fogMap2[y][x] = fogMap[y][x];
+		}
+	}
+
+	//Check portal 1 position for rendering
+	if (_portal.p_isActive[0])
+	{
+		for (short p_x = _portal.p_pos[0].X - portalViewRange - 1; p_x < _portal.p_pos[0].X + portalViewRange + 1; p_x++)
+		{
+			for (short p_y = _portal.p_pos[0].Y - portalViewRange; p_y < _portal.p_pos[0].Y + portalViewRange - 2; p_y++)
+			{
+				if (p_x < 0)
+				{
+					continue;
+				}
+				if (p_y < 0)
+				{
+					continue;
+				}
+
+				fogMap[p_y][p_x] = g_mapData.mapGrid[p_y][p_x];
+				fogMap2[p_y][p_x] = fogMap[p_y][p_x];
+			}
+		}
+	}
+
+	//Check portal 2 position for rendering
+	if (_portal.p_isActive[1])
+	{
+		for (short p_x = _portal.p_pos[1].X - portalViewRange - 1; p_x < _portal.p_pos[1].X + portalViewRange + 1; p_x++)
+		{
+			for (short p_y = _portal.p_pos[1].Y - portalViewRange; p_y < _portal.p_pos[1].Y + portalViewRange - 2; p_y++)
+			{
+				if (p_x < 0)
+				{
+					continue;
+				}
+				if (p_y < 0)
+				{
+					continue;
+				}
+
+				fogMap[p_y][p_x] = g_mapData.mapGrid[p_y][p_x];
+				fogMap2[p_y][p_x] = fogMap[p_y][p_x];
+			}
+		}
+	}
+
+	//Check bullet position for rendering
+	if (_bullet.b_isActive)
+	{
+		for (short b_x = _bullet.b_pos.X - bulletViewRange - 1; b_x < _bullet.b_pos.X + bulletViewRange + 1; b_x++)
+		{
+			for (short b_y = _bullet.b_pos.Y - bulletViewRange; b_y < _bullet.b_pos.Y + bulletViewRange; b_y++)
+			{
+				if (b_x < 0)
+				{
+					continue;
+				}
+				if (b_y < 0)
+				{
+					continue;
+				}
+
+				fogMap[b_y][b_x] = g_mapData.mapGrid[b_y][b_x];
+				fogMap2[b_y][b_x] = fogMap[b_y][b_x];
+			}
 		}
 	}
 
