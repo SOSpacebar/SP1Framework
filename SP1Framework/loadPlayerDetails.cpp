c67d0526a72057a476d7696458e0d764fe7e5ec4
Ng Jing Jie <ex-milobur@hotmail.com>
2016-08-25 16:38:00 +0800

    Added WIP files for loading player data.

---
+++ SP1Framework/loadPlayerDetails.cpp
@@ -0,0 +1,12 @@
+#include "loadPlayerDetails.h"
+
+#include <fstream>
+
+void readFile()
+{
+	ifstream playerData;
+
+	int tempStorage[8];
+
+	//playerData.open()
+}
---
+++ SP1Framework/loadPlayerDetails.h
@@ -0,0 +1,11 @@
+#ifndef _LOADPLAYERDETAILS_H
+#define _LOADPLAYERDETAILS_H
+
+#include "game.h"
+#include "playerStats.h"
+
+
+void readFile();
+
+
+#endif
